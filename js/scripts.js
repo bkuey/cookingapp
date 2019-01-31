63d0a24b958d2b7574ffee10091411b160a9789b
Mai Kue Lor and Paula Vun <mai@>
2019-01-30 20:38:30 -0800

    created cookingapp.html and linked scripts.js to include measurement conversions.

---
+++ js/scripts.js
@@ -0,0 +1,95 @@
+var tspTbsp= function(number1){
+return (number1 / 3)
+};
+
+var number1 = parseInt(prompt("Teaspoon to Tablespoon"));
+var result = tspTbsp(number1);
+alert(result);
+
+var tbspTsp= function(number1){
+return (number1 * 3)
+};
+
+var number1 = parseInt(prompt("Tablespoon to Teaspoon"));
+var result = tbspTsp(number1);
+alert(result);
+
+var tbspCups= function(number1){
+return (number1 / 16)
+};
+
+var number1 = parseInt(prompt("Tablespoon to Cups"));
+var result = tbspCups(number1);
+alert(result);
+
+var cupsTbsp= function(number1){
+return (number1 * 16)
+};
+
+var number1 = parseInt(prompt("Cups to Tablespoon"));
+var result = cupsTbsp(number1);
+alert(result);
+
+var cupsPint= function(number1){
+return (number1 / 2)
+};
+
+var number1 = parseInt(prompt("Cups to Pints"));
+var result = cupsPint(number1);
+alert(result);
+
+var pintCups= function(number1){
+return (number1 * 2)
+};
+
+var number1 = parseInt(prompt("Pints to Cups"));
+var result = pintCups(number1);
+alert(result);
+
+var cupQuart= function(number1){
+return (number1 / 4)
+};
+
+var number1 = parseInt(prompt("Cups to Quarts"));
+var result = cupQuart(number1);
+alert(result);
+
+var quartCup= function(number1){
+return (number1 * 4)
+};
+
+var number1 = parseInt(prompt("Quarts to Cups"));
+var result = quartCup(number1);
+alert(result);
+
+var quartGallon= function(number1){
+return (number1 / 4)
+};
+
+var number1 = parseInt(prompt("Quarts to Gallons"));
+var result = quartGallon(number1);
+alert(result);
+
+var gallonQuart= function(number1){
+return (number1 * 4)
+};
+
+var number1 = parseInt(prompt("Gallons to Quarts"));
+var result = gallonQuart(number1);
+alert(result);
+
+var gallonLiter= function(number1){
+return (number1 * 3.78541)
+};
+
+var number1 = parseInt(prompt("Gallons to Liter"));
+var result = gallonLiter(number1);
+alert(result);
+
+var literGallon= function(number1){
+return (number1 / 3.78541)
+};
+
+var number1 = parseInt(prompt("Liters to Gallons"));
+var result = literGallon(number1);
+alert(result);
